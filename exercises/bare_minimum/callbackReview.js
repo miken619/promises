80dac99cbb9da53a8e2214e6c1517602e4798141
Hack Reactor Student <student@hackreactor.com>
2017-07-19 16:56:41 -0700

    Fullfilled test requirement

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,30 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {   
   // TODO
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {     
+      cb(err);
+    } else {      
+      var firstLine = content.slice(0, content.indexOf('\n'));
+      cb(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, cb) {
   // TODO
+  request(url, function(error, response, body) {
+    
+    if (error) {
+      cb(error);
+    } else {
+      cb(null, response.statusCode);
+    }
+    
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
